81a760b9ea1464647e347b1a32427dff8da59449
hemant1883 <167603000+hemant1883@users.noreply.github.com>
2026-08-08 12:04:02 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api'
+  baseURL: import.meta.env.VITE_API_URL + '/api'
 });
 
-// Interceptor to add JWT to every request
 api.interceptors.request.use((config) => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.token) {
-        config.headers.Authorization = `Bearer ${user.token}`;
-    }
-    return config;
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (user && user.token) {
+    config.headers.Authorization = `Bearer ${user.token}`;
+  }
+
+  return config;
 }, (error) => {
-    return Promise.reject(error);
+  return Promise.reject(error);
 });
 
 export default api;
